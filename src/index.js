0ac566babe272f4eb77ac30c2a68961905a60156
SergiyRybin <serhiobax@gmail.com>
2022-04-22 09:47:33 +0300

    upd

--- src/index.js
+++ src/index.js
@@ -1,31 +1,36 @@
 import './sass/main.scss';
-// import simpleLightbox from 'simplelightbox';
 import Notiflix from 'notiflix';
 import axios from 'axios';
-import * as basicLightbox from 'basiclightbox'
 
-
+// import * as basicLightbox from 'basiclightbox'
+// import simpleLightbox from 'simplelightbox';
 // Notiflix.Notify.success( 'Message');
+
+
 let nameImage = '';
 let pageCount = 1;
 const searchBox = document.querySelector("#search-form")
 const boxImage = document.querySelector(".gallery")
+const loadMoreBtn = document.querySelector(".load-more")
+
+loadMoreBtn.disabled = true
 
 searchBox.addEventListener("submit", onText)
 
 function onText(event){
+
     event.preventDefault()
-    // listCountry.innerHTML='';
+
     nameImage = event.currentTarget.elements.searchQuery.value
- 
-    fetchUser(nameImage).then(renderImage).catch()
-    pageCount+=1
-    // .catch(Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again."))
+    fetchUser(nameImage)
+    .then(renderImage)
+    .then(loadMore)
   }
-
-  // {webformatURL, largeImageURL, tags, likes, views, comments }
-
   function renderImage({data, data:{hits}}){
+    if(data.totalHits===0){
+     return Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.")
+    }
+
     const markup = hits.map(({webformatURL, largeImageURL, tags, likes, views, comments, downloads})=>{
       return `<div class="photo-card"><img src=${webformatURL} alt="" loading="lazy" /><div class="info">
         <p class="info-item">
@@ -45,33 +50,18 @@ function onText(event){
     }).join("")
 
     boxImage.insertAdjacentHTML("beforeend", markup)
-  // boxImage.innerHTML(markup)
 
-// let galleryModal = new simpleLightbox('.gallery .photo-card', {
-//   caption: true,
-//   captionsData: 'alt',
-//   captionDelay: 250,
-//   close: true,
-//   closeText: "&#x2612;",
-//   nav: true,
-//   navText: ['&#8656;','&#8658;']
-// })
-}
-function fetchUser(elements){
-  return axios.get(`https://pixabay.com/api/?key=26842209-8060593a7142b471474d704cf&q=${elements}&image_type=photo&orientation=horizontal&safesearch=true&page=${pageCount}&per_page=40`)
-}
-function renderError(){
-  return 
+    loadMoreBtn.disabled = false
+    pageCount+=1
+
 }
 
-function openModal (event){
-//  event.addEventListener
-// const card =  document.querySelector(".photo-card")
-  // console.log(event.currentTarget)
+async function fetchUser(elements){
+    return await axios.get(`https://pixabay.com/api/?key=26842209-8060593a7142b471474d704cf&q=${elements}&image_type=photo&orientation=horizontal&safesearch=true&page=${pageCount}&per_page=10`)
+}
 
-  // const intanse = basicLightbox.create(`<div class="modal">
-  // <img src=${data.hits.largeImageURL} width = 900vw >
-  // </div>
-  // `)
-  // intanse.show()
+ function loadMore(){
+  loadMoreBtn.addEventListener("click", ()=>{
+    fetchUser(nameImage).then(renderImage)
+  })
 }
