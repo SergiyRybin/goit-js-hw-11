dac7df47cf75d40a34354c6719c7caa99be47840
SergiyRybin <serhiobax@gmail.com>
2022-04-26 20:00:12 +0300

    grid

--- src/index.js
+++ src/index.js
@@ -43,7 +43,8 @@ async function renderImage({ data, data: { hits } }) {
     .map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => {
       return `<div class="photo-card"><img src=${webformatURL} alt="${tags}" loading="lazy" /><div class="info">
         <p class="info-item">
-          <b>Likes</b> ${likes}
+          <b>Likes</b> 
+          ${likes}
         </p>
         <p class="info-item">
           <b>Views</b> ${views}
